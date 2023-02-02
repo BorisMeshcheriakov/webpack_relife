8bef6cd3e08e3f08d225fef40abe9e2d6b439a07
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-02 23:19:02 +0300

    feat: Sass file structure

--- src/App.tsx
+++ src/App.tsx
@@ -16,22 +16,6 @@ const App: React.FC = () => {
         iusto omnis quo labore possimus officiis? Sint repudiandae beatae exercitationem non nam
         voluptas nostrum praesentium, nisi inventore aliquam officia libero accusantium commodi
         neque rerum explicabo. Ab, tempora? Nihil porro qui dolorem officia labore beatae fuga quos.
-        Illum reprehenderit distinctio ipsam sequi quia quisquam fugit, ipsum deserunt? Voluptatibus
-        doloremque, autem porro aliquid quo perferendis voluptatum explicabo eveniet, ea possimus
-        eaque exercitationem ducimus. Quam aspernatur numquam optio eligendi. Alias sapiente nulla,
-        at suscipit facilis ducimus expedita quia natus totam officia nisi voluptate officiis
-        deserunt nostrum pariatur, odit blanditiis autem saepe, dicta voluptatibus! Ab nisi ullam
-        dolore animi debitis. Quae doloribus voluptate ex, sint, dolores optio aperiam labore
-        obcaecati et aliquid, voluptatem eligendi atque? Deserunt temporibus ratione atque ipsa
-        minus, soluta deleniti suscipit, nobis fugit voluptate repellendus, recusandae illo. Eum id
-        tenetur doloribus quidem odit ipsa explicabo vero impedit necessitatibus, maiores, est neque
-        illum alias commodi, veritatis esse molestias error sapiente laboriosam omnis assumenda
-        quia! Est omnis voluptates debitis? Placeat laborum deleniti facere adipisci, numquam
-        nostrum enim maiores odit provident, dolorem ex eveniet itaque. Sit perspiciatis ea ut
-        doloremque magni vero nihil, autem repellendus nostrum, quod nulla, iusto praesentium? Nulla
-        quibusdam perspiciatis amet voluptate sed in! Ut voluptate veniam quis qui officiis illum
-        voluptatem. Doloremque eum cupiditate voluptatibus sint laboriosam reprehenderit vel velit
-        obcaecati itaque, molestiae sit harum quisquam!
       </header>
     </div>
   );
