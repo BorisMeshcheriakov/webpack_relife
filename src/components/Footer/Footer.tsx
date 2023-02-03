f1159466689504ea3805239cbe6f137a7e750654
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-03 17:58:19 +0300

    feat: Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import st from './Footer.module.scss';
+
+const Footer: React.FC = () => {
+  return <footer className={st.footer}>Footer</footer>;
+};
+
+export default Footer;
