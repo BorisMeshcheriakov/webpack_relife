a0fa47943cd536b545ddc80129ac618c06953555
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-03 17:05:19 +0300

    feat: Font size variables, default size for h1 blocks

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,19 @@ const App: React.FC = () => {
   return (
     <div className={st.app}>
       <header className={st.header}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates voluptatem facere,
-        reprehenderit, tempora odio dolorum perspiciatis neque voluptate nesciunt voluptas quam
-        cumque iste nam, minima nisi mollitia nulla cupiditate. Aliquam. Enim aperiam distinctio
-        exercitationem quos error? Temporibus ullam animi tempore laudantium ab, fuga doloribus
-        reprehenderit debitis sint voluptates dicta et vel qui illum nesciunt atque in. Facilis
-        dicta quo ad. Ratione alias accusamus saepe tempora aut labore, facere culpa, quisquam, id
-        ut error. Temporibus consequatur alias porro nostrum et expedita ipsum eaque ut, veniam
-        iusto omnis quo labore possimus officiis? Sint repudiandae beatae exercitationem non nam
-        voluptas nostrum praesentium, nisi inventore aliquam officia libero accusantium commodi
-        neque rerum explicabo. Ab, tempora? Nihil porro qui dolorem officia labore beatae fuga quos.
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates voluptatem facere,
+          reprehenderit, tempora odio dolorum perspiciatis neque voluptate nesciunt voluptas quam
+          cumque iste nam, minima nisi mollitia nulla cupiditate. Aliquam.
+        </h1>
       </header>
+
+      <h1>H1 header</h1>
+      <h2>H2 header</h2>
+      <h3>H3 header</h3>
+      <h4>H4 header</h4>
+      <h5>H5 header</h5>
+      <h6>H6 header</h6>
     </div>
   );
 };
