a679537b9b96aefda4ef8f182aa95499f6a7fd2b
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-02-04 15:39:46 +0300

    feat: Prettier - use tabs

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,31 @@ import { Footer } from './components';
 import st from './App.scss';
 
 const App: React.FC = () => {
-  return (
-    <div className={st.app}>
-      <header className={st.header}>
-        <h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates voluptatem facere,
-          reprehenderit, tempora odio dolorum perspiciatis neque voluptate nesciunt voluptas quam
-          cumque iste nam, minima nisi mollitia nulla cupiditate. Aliquam.
-        </h1>
-      </header>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia deleniti, dolore beatae
-          sit atque aliquam eius asperiores iste adipisci cupiditate aperiam praesentium vero. At
-          fuga suscipit nulla a? Laboriosam, dolores.
-        </p>
-        <p>
-          <button>Button</button>
-        </p>
-        <p>
-          <a href="">Link</a>
-        </p>
-      </div>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className={st.app}>
+			<header className={st.header}>
+				<h1>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates voluptatem facere,
+					reprehenderit, tempora odio dolorum perspiciatis neque voluptate nesciunt voluptas quam
+					cumque iste nam, minima nisi mollitia nulla cupiditate. Aliquam.
+				</h1>
+			</header>
+			<div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia deleniti, dolore beatae
+					sit atque aliquam eius asperiores iste adipisci cupiditate aperiam praesentium vero. At
+					fuga suscipit nulla a? Laboriosam, dolores.
+				</p>
+				<p>
+					<button>Button</button>
+				</p>
+				<p>
+					<a href="">Link</a>
+				</p>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
