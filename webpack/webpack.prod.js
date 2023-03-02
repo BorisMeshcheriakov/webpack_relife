6844f770aba3dae208551a386fc6c2a3de9ebb8d
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-03-02 12:30:38 +0300

    fix: ts target, clean unnecessary imports

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,14 +1,16 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const { paths } = require('./configuration');
+
 const {
 	miniCssExtractPlugin,
 	cleanWebpackPlugin,
 	imageMinimizerWebpackPlugin,
 	esbuildPlugin,
 } = require('./plugins');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
+
+// Заменяем на esbuild plugin
+// const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
 
 const plugins = [cleanWebpackPlugin, miniCssExtractPlugin];
 
