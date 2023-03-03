6e98af6ed6780b236150afda5d5137151378efd1
meshcheriakov.b <meshcheriakov.b@nicecode.biz>
2023-03-03 11:14:16 +0300

    feat: preview mode, disable react dev tools for production

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
 import App from './App';
+
+if (process.env.NODE_ENV === 'production') {
+	disableReactDevTools();
+}
 
 ReactDOM.render(
 	<React.StrictMode>
